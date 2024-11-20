4d582e4d935867c9cb6ac0039cd38aee2a2953f6
elisa.zyj <elisa.zyj@outlook.com>
2024-11-20 16:05:37 +0800

    feat: 更新 vue.config、package.json

--- vue-src/src/main.js
+++ vue-src/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+{{console.log({
+  'NODE_ENV': process.env.NODE_ENV,
+  'VUE_APP_MODE': process.env.VUE_APP_MODE,
+  'BASE_URL': process.env.BASE_URL
+})}}
 createApp(App).mount('#app')
