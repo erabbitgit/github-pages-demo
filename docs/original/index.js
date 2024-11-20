38faffbe359321a786658f2f4d7f05f67ba1e1ae
elisa.zyj <elisa.zyj@outlook.com>
2024-11-20 16:10:59 +0800

    build: 重產

--- docs/original/index.js
+++
@@ -1,3 +0,0 @@
-window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, this is github pages demo  ʕ •ᴥ• ʔ'
-}
--- vue-src/vue.config.js
+++ vue-src/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/', // 依環境設置，指定生產環境的靜態文件的公共路徑
-  outputDir: path.resolve(__dirname, '../docs/vue-project'), // 編譯結果放到 /docs/vue-project
+  outputDir: path.resolve(__dirname, '../docs/'), // 編譯結果放到 /docs/vue-project
   configureWebpack: {
     resolve: {
       alias: {
