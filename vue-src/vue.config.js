10b0b5fc044a2f4d88bea5be57fc62c275ac6519
elisa.zyj <elisa.zyj@outlook.com>
2024-11-20 16:46:00 +0800

    test: 更換vue config publicPath、build重產

--- vue-src/vue.config.js
+++ vue-src/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/', // 依環境設置，指定生產環境的靜態文件的公共路徑
+  publicPath: '/',
   outputDir: path.resolve(__dirname, '../docs/vue-project'), // 編譯結果放到 /docs/vue-project
   configureWebpack: {
     resolve: {
