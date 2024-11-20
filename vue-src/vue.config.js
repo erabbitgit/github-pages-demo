85abad259c9ea5d6e8116dc62a5efb91d3b36cde
elisa.zyj <elisa.zyj@outlook.com>
2024-11-20 16:23:43 +0800

    test: build資料改放回docs/vue-project底下

--- vue-src/vue.config.js
+++ vue-src/vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/', // 依環境設置，指定生產環境的靜態文件的公共路徑
-  outputDir: path.resolve(__dirname, '../docs/'), // 編譯結果放到 /docs/vue-project
+  publicPath: process.env.NODE_ENV === 'production' ? '/github-pages-demo/vue-project/' : '/', // 依環境設置，指定生產環境的靜態文件的公共路徑
+  outputDir: path.resolve(__dirname, '../docs/vue-project'), // 編譯結果放到 /docs/vue-project
   configureWebpack: {
     resolve: {
       alias: {
